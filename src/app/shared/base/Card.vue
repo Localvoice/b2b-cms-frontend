6babfe70d94f0da7609c75424bae2d7f77f84d45
Filip <filip.knias@o2.pl>
2025-05-23 15:18:44 +0200

    fix container card class

--- src/app/shared/base/Card.vue
+++ src/app/shared/base/Card.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-card class="pa-8" :color="layoutState.layoutMode" :dark="layoutState.isDarkMode" max-width="100%" rounded="lg">
+  <v-card
+    class="pa-8 ml-auto mr-auto"
+    :color="layoutState.layoutMode"
+    :dark="layoutState.isDarkMode"
+    max-width="1800px"
+    rounded="lg"
+  >
     <v-skeleton-loader height="100%" width="100%" type="card" :loading="loading">
       <slot />
     </v-skeleton-loader>
